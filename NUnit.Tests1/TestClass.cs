7c6a2e8dd41624e28be1ea3d7f7b53e2f1020e64
Ivan Lesnikov <iv.al.lesnikov@gmail.com>
2018-08-02 18:37:10 +0300

    заливка для нюка

--- NUnit.Tests1/TestClass.cs
+++ NUnit.Tests1/TestClass.cs
@@ -4,17 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Chrome;
 
 namespace NUnit.Tests1
 {
     [TestFixture]
     public class TestClass
     {
+        private IWebDriver Cdriver;
+        private String URL = "https://yandex.ru/";
+
         [Test]
         public void TestMethod()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            Cdriver = new ChromeDriver();
+            Cdriver.Navigate().GoToUrl(URL);
+        }
+        public void cleanup()
+        {
+            Cdriver.Quit();
         }
     }
 }

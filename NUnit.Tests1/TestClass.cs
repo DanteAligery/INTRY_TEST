a076084d4225c4cabf5ce352b3a31aa72f45fb62
Ivan Lesnikov <iv.al.lesnikov@gmail.com>
2018-08-07 18:04:54 +0300

    api login
    пока возвращает 401

--- NUnit.Tests1/TestClass.cs
+++ NUnit.Tests1/TestClass.cs
@@ -44,27 +44,19 @@ namespace NUnit.Tests1
             String basicAUTHencoded = System.Convert.ToBase64String(data);
             
             HttpClient client = new HttpClient();
-            //client.BaseAddress = mainUri;
-            //client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", basicAUTHencoded);
-
-            //var headers = client.DefaultRequestHeaders;
-            client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            
+            //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
             client.DefaultRequestHeaders.Add("WWW-Authenticate", "NTLM");
             client.DefaultRequestHeaders.Add("WWW-Authenticate", "Basic" + " " + basicAUTHencoded);
+            
             HttpResponseMessage response = new HttpResponseMessage();
+           
             response = await client.GetAsync(mainUri);
+
             response.EnsureSuccessStatusCode();
-            string responseBody = await response.Content.ReadAsStringAsync();
-
-            //var response = client.GetStringAsync(mainUri).Result;
-            //String responseContent = response.ToString();
-            //Console.WriteLine(responseContent);
-
-            //server server = new server(@"C:\Users\dante\Documents\soft\browsermob-proxy-2.1.4\bin\browsermob-proxy.bat");
-            //Cdriver = new ChromeDriver();
-            //Cdriver.Navigate().GoToUrl(URL);
-            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
-
+            HttpContent content = response.Content;
+            string result = await content.ReadAsStringAsync();
+            
         }
         public void cleanup()
         {

5bb357932dd22a3bbc0b20d4f4283218332a06ed
Ivan Lesnikov <iv.al.lesnikov@gmail.com>
2018-08-09 17:51:13 +0300

    1st test gui

---
+++ NUnit.Tests1/GUI/TestClass1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1.GUI
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver();
+
+
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            driver.Url = "http://test-squadspace.squadsoft.ru/default.aspx/";
+            // TODO: Add your test code here
+            //Assert.Pass("Your first passing test");
+        }
+        [TearDown]
+        public void close()
+        {
+            driver.Close();
+        }
+    }
+}
